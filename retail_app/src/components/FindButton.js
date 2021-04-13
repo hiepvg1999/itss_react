79f3bf24eb84704d7581ed9e14e91a362999ea7e
hiepvg1999 <hiepbkcntt@gmail.com>
2021-04-13 15:44:41 +0000

    find button

---
+++ retail_app/src/components/FindButton.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import showFindResult from './showFindResult'
+import { Button, Form, FormGroup, Input, Row, Col } from 'reactstrap';
+
+class FindButton extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      findProduct: '',
+      resultFindProduct: []
+    };
+    this.handleChange = this.handleChange.bind(this)
+    this.handleFindProduct = this.handleFindProduct.bind(this)
+}
+  
+  handleChange(event) {
+    this.setState({findProduct: event.target.value})
+  }
+
+  handleFindProduct(event){
+    const products = this.props.products;
+    const findProduct = this.state.findProduct;
+    for(let i = 0; i < products.length; i++){
+      if(products[i].description === findProduct){
+        this.state.resultFindProduct.push(products[i]);
+        this.setState({resultFindProduct: this.state.resultFindProduct});
+      }
+    }
+    this.setState({findProduct: ''})
+    event.preventDefault()
+  }
+
+  handleDelete = (index) => {
+    this.state.resultFindProduct.splice(index, 1)
+    this.setState({resultFindProduct: this.state.resultFindProduct})
+  }
+
+  render() {
+    var resultShow = this.state.resultFindProduct.map((result, key) => (
+      <ShowFindResult
+        key = {key}
+        result= {result}
+        onDelete = {() => this.handleDelete(key)}
+      />
+    ))
+
+    return (
+      <div>
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }}>
+            <h1>Kanji - Click and Learn</h1>
+            <Form>
+              <FormGroup row>
+                <Col sm={10}>
+                <Input 
+                  type="text" 
+                  name="inputword" 
+                  placeholder="Search a product by description"
+                  value={this.state.findProduct}
+                  onChange={this.handleChange}
+                ></Input>
+                </Col>
+                <Button color="warning" onClick={this.handleFindProduct}>Search</Button>
+              </FormGroup>
+            </Form>
+            
+          </Col>
+        </Row>
+        <div>
+        {resultShow}
+        </div>
+      </div >
+    );
+  }
+}
+
+export default FindButton;
